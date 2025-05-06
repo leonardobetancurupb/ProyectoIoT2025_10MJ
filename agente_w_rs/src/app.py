508b617b14f9ec343d3d70db1a70069605122abc
mpaulazzv <mariapaulaflorezvargas11@gmail.com>
2025-05-06 09:06:53 -0500

    agente + sensor completo

--- agente_w_rs/src/app.py
+++ agente_w_rs/src/app.py
@@ -1,23 +1,33 @@
-import flask
+import os
 from flask import Flask, request, jsonify
 import json
-
-
-## App de Flask
 
 app = Flask(__name__)
 
 @app.route("/recibir", methods=["POST"])
 def getData():
-    
+    ruta_directorio = "data"
+    ruta_archivo = os.path.join(ruta_directorio, "agente_w_rs.json")
+    nuevos_datos = request.get_json()
+
     try:
-        data = request.get_json()
-        
-        with open('/data/agente_w_rs.json', 'w') as archivo_json:
-            json.dump(data, archivo_json, indent=3)
-        
-        return jsonify({"mensaje": "Ok", "data": data})
-    
+        os.makedirs(ruta_directorio, exist_ok=True)
+        if os.path.exists(ruta_archivo):
+            with open(ruta_archivo, 'r') as archivo_json:
+                datos_existentes = json.load(archivo_json)
+        else:
+            datos_existentes = []
+
+        if not isinstance(datos_existentes, list):
+            datos_existentes = [datos_existentes]
+
+        datos_existentes.append(nuevos_datos)
+
+        with open(ruta_archivo, 'w') as archivo_json:
+            json.dump(datos_existentes, archivo_json, indent=3)
+
+        return jsonify({"mensaje": "Ok", "data": nuevos_datos})
+
     except Exception as e:
         return jsonify({"mensaje": "Error", "error": str(e)}), 400
 
