cda1c583ef0ebd64ba99dbf3fce564a224c89bc2
adaima <luaesz93@gmail.com>
2025-05-29 05:14:37 -0500

    Actualización urls para añadir imagenes

--- project/config/urls.py
+++ project/config/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +10,8 @@ urlpatterns = [
     path('sensors/', include('sensors.urls')),
     path('', include('landing.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'landing/static'))
+
+
